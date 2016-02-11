caa93ef3f88f66b6faaf6dfdb4504af7d8398094
elzat <collet.e@hotmail.fr>
2016-02-11 08:47:46 +0100

    Main
    J'ai des soucis avec les commit et les pulls je modifie directement le fichier.

--- La pipopipette/src/HelloWorld.java
+++ La pipopipette/src/HelloWorld.java
@@ -2,54 +2,16 @@
 public class HelloWorld {
 	
 	public static void main(String [] args) {
-		System.out.println("Hello world!");
-		System.out.println("Hello world!");
-		System.out.println("Elsa!");
-		System.out.println("Hello world ! This is me, Younes");
-		affichagePlateau();
-		System.out.println("Fini !bfgf");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Saisir le nombre de lignes : ");
+		int nbLigne= sc.nextInt();
+		System.out.println("Saisir le nombre de colonnes : ");
+		int nbColonne= sc.nextInt();
+		System.out.println("Saisir le numéro de configuration : \n\t-1 : Avec bordures\n\t-2 : Sans bordure");
+		int configuration= sc.nextInt();
+		System.out.println("\n nbLigne ="+nbLigne+"\n nbColonne = "+nbColonne+"\nnbConf "+configuration);
+		Plateau plateau = new Plateau(nbLigne, nbColonne, configuration);
+		System.out.println(plateau.toString());
 		
 	}
-
-	
-	public static void affichagePlateau(){
-		String[] ligne= new String[4];
-		String[] colonne= new String[4];
-		String a="111";
-		ligne[0]=a;
-		String b="000";
-		ligne[1]=b;
-		String c="000";
-		ligne[2]=c;
-		String d="111";
-		ligne[3]=d;
-		colonne[0]=a;
-		colonne[1]=b;
-		colonne[2]=c;
-		colonne[3]=d;
-		
-		for(int i=0; i<ligne.length; i++){
-			System.out.print(".");
-			for(int j=0; j<ligne[i].length(); j++){
-				String carac= ligne[i].substring(j, j+1);
-				if(carac.equals("1")){
-					System.out.print("_");
-				}
-				else{
-					System.out.print(" ");
-				}
-			}
-			for(int j=0; j<colonne[i].length(); j++){
-				String carac= colonne[i].substring(j, j+1);
-				if(carac.equals("1")){
-					System.out.print("|");
-				}
-				else{
-					System.out.print(" ");
-				}
-			}
-		}
-	}
-	
-	
 }
